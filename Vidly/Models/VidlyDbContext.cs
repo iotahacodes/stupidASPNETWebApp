14c4ab9333c7ed2b7e3b8b1ae4910065c6566191
Taha Farahani <iotahacodes@gmail.com>
2020-08-26 18:12:49 +0430

    codeFirst Database

---
+++ Vidly/Models/VidlyDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Vidly.Models;
+
+namespace Oceans.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public AppDbContext()
+        {
+            AppDomain.CurrentDomain.SetData("DataDirectory", System.IO.Directory.GetCurrentDirectory());
+        }
+    }
+}
